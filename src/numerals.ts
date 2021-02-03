d603b35205d27bfc0411b669ca14a85677cf3b9b
Chris McKirgan <chris@mckirgan.com>
2021-02-03 19:36:04 +0000

    Added; working conversion for 4 & 5

--- src/numerals.ts
+++ src/numerals.ts
@@ -1,9 +1,21 @@
 export const convertNumerals = (givenInt: number) : string => {
-
     let romanNumeral: string = '';
+    let remainValue: number = givenInt;
 
-    for (let i=0; i<givenInt; i++) {
+    if (remainValue == 4) {
+        romanNumeral += 'IV'
+        remainValue -= 4;
+    }
+
+    if (remainValue >= 5) {
+        romanNumeral += 'V'
+        remainValue -= 5;
+    }
+
+
+    while (remainValue > 0) {
         romanNumeral += 'I';
+        remainValue -= 1;
     }
 
     return romanNumeral;
