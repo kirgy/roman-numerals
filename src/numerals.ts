ee87430957cbb2c404a20286dd5095f0b2f0f290
Chris McKirgan <chris@mckirgan.com>
2021-02-03 19:49:12 +0000

    Changed; convertNumerals to convert numbers up to 11
    I realised I essentially could achieve what I have coded more effectively in a loop.

--- src/numerals.ts
+++ src/numerals.ts
@@ -1,22 +1,22 @@
+interface intRomDef {
+    int: number,
+    rom: string,
+}
+
 export const convertNumerals = (givenInt: number) : string => {
     let romanNumeral: string = '';
     let remainValue: number = givenInt;
 
-    if (remainValue == 4) {
-        romanNumeral += 'IV'
-        remainValue -= 4;
-    }
-
-    if (remainValue >= 5) {
-        romanNumeral += 'V'
-        remainValue -= 5;
-    }
-
-
-    while (remainValue > 0) {
-        romanNumeral += 'I';
-        remainValue -= 1;
-    }
+    const intToRom: Array<intRomDef> = [
+        {int:10, rom:'X'}, {int:9, rom:'IX'}, {int:5, rom:'V'}, {int:4, rom:'IV'}, {int:1, rom:'I'}, 
+    ];
+    
+    intToRom.forEach(intRom => {
+        while (remainValue > intRom.int-1) {
+            romanNumeral += intRom.rom;
+            remainValue -= intRom.int;
+        }        
+    });
 
     return romanNumeral;
 }
