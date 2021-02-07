411d81932ee7a0de78cd4ae269ea00186cc54f25
Chris McKirgan <chris@mckirgan.com>
2021-02-07 13:15:02 +0000

    Changed; tests to be more legible
    As tests were essentially the same but with varying input-output pairs the large file has actually become less legible, so using a test itterator and an array helps future devs better understand what is being achieved here.

--- tests/src/numerals.test.ts
+++ tests/src/numerals.test.ts
@@ -5,82 +5,23 @@ import { convertNumerals } from "../../src/numerals"
  * http://www.novaroma.org/via_romana/numbers.html
  */
 
-test('converts decimal 1 to roman numeral I', () => {
-    expect(convertNumerals(1)).toBe('I');
-});
+const testCases = [[1,'I'], [2,'II'], [4,'IV'], [5,'V'], [6,'VI'], [9,'IX'], [10,'X'], 
+[11,'XI'], [14,'XIV'], [15,'XV'], [16,'XVI'], [17,'XVII'], [19,'XIX'], [20,'XX'], 
+[21,'XXI'], [49,'XLIX'], [50,'L'], [51,'LI'], [55,'LV'], [60,'LX']]
 
-test('converts decimal 2 to roman numeral II', () => {
-    expect(convertNumerals(2)).toBe('II');
-});
+test.each(testCases)(
+    'converts decimal %i to roman numeral %s',
+    (testInt, romanNumeral) => {
+        /**
+         * the callback is giving us string|number, but our typescript convertNumerals only expects
+         * a number, so we're going to throw an Error in case someone has set up a test wrong. Otherwise
+         * we get a compile error. There is probably a better way of doing this, but time constraints 
+         * are limiting me here.
+         */
 
-test('converts decimal 4 to roman numeral IV', () => {
-    expect(convertNumerals(4)).toBe('IV');
-});
-
-test('converts decimal 5 to roman numeral V', () => {
-    expect(convertNumerals(5)).toBe('V');
-});
-
-test('converts decimal 6 to roman numeral VI', () => {
-    expect(convertNumerals(6)).toBe('VI');
-});
-
-test('converts decimal 9 to roman numeral IX', () => {
-    expect(convertNumerals(9)).toBe('IX');
-});
-
-test('converts decimal 10 to roman numeral X', () => {
-    expect(convertNumerals(10)).toBe('X');
-});
-
-test('converts decimal 11 to roman numeral XI', () => {
-    expect(convertNumerals(11)).toBe('XI');
-});
-
-test('converts decimal 14 to roman numeral XIV', () => {
-    expect(convertNumerals(14)).toBe('XIV');
-});
-
-test('converts decimal 15 to roman numeral XV', () => {
-    expect(convertNumerals(15)).toBe('XV');
-});
-
-test('converts decimal 16 to roman numeral XVI', () => {
-    expect(convertNumerals(16)).toBe('XVI');
-});
-
-test('converts decimal 17 to roman numeral XVII', () => {
-    expect(convertNumerals(17)).toBe('XVII');
-});
-
-test('converts decimal 19 to roman numeral XIX', () => {
-    expect(convertNumerals(19)).toBe('XIX');
-});
-
-test('converts decimal 20 to roman numeral XX', () => {
-    expect(convertNumerals(20)).toBe('XX');
-});
-
-test('converts decimal 21 to roman numeral XXI', () => {
-    expect(convertNumerals(21)).toBe('XXI');
-});
-
-test('converts decimal 49 to roman numeral XLIX', () => {
-    expect(convertNumerals(49)).toBe('XLIX');
-});
-
-test('converts decimal 50 to roman numeral L', () => {
-    expect(convertNumerals(50)).toBe('L');
-});
-
-test('converts decimal 51 to roman numeral LI', () => {
-    expect(convertNumerals(51)).toBe('LI');
-});
-
-test('converts decimal 55 to roman numeral LV', () => {
-    expect(convertNumerals(55)).toBe('LV');
-});
-
-test('converts decimal 60 to roman numeral LX', () => {
-    expect(convertNumerals(60)).toBe('LX');
-});
+        if (typeof(testInt) === 'string') {
+            throw new Error('Cannot run test on type of string to Roman numeral');
+        }
+        expect(convertNumerals(testInt)).toBe(romanNumeral);
+    },
+  );
