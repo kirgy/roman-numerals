faecd1aaf539bd079303b6da7c1dc93f4d392edf
Chris McKirgan <chris@mckirgan.com>
2021-02-07 13:24:41 +0000

    Added; tests for area of integer 500

--- tests/src/numerals.test.ts
+++ tests/src/numerals.test.ts
@@ -8,7 +8,8 @@ import { convertNumerals } from "../../src/numerals"
 const testCases = [[1,'I'], [2,'II'], [4,'IV'], [5,'V'], [6,'VI'], [9,'IX'], [10,'X'], 
 [11,'XI'], [14,'XIV'], [15,'XV'], [16,'XVI'], [17,'XVII'], [19,'XIX'], [20,'XX'], 
 [21,'XXI'], [49,'XLIX'], [50,'L'], [51,'LI'], [55,'LV'], [60,'LX'], [90,'XC'],
-[100,'C'], [101,'CI'], [104,'CIV'], [105,'CV'], [110,'CX'], [140,'CXL'], [150,'CL']]
+[100,'C'], [101,'CI'], [104,'CIV'], [105,'CV'], [110,'CX'], [140,'CXL'], [150,'CL'],
+[400,'CD'], [490,'CDXC'], [500,'D'], [501,'DI'], [505,'DV'], [510,'DX'], [600,'DC']]
 
 test.each(testCases)(
     'converts decimal %i to roman numeral %s',
