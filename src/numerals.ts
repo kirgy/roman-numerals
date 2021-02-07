36557db0a32253c548c90d5feeee05486fa73d64
Chris McKirgan <chris@mckirgan.com>
2021-02-07 13:29:27 +0000

    Added; working conversion for area of int 500
    Added a comment for clarity, as it may not be obvious that the ordering of the array is important, and it doesn't seem an efficient idea to do a sort in code to protect against this.

--- src/numerals.ts
+++ src/numerals.ts
@@ -7,9 +7,13 @@ export const convertNumerals = (givenInt: number) : string => {
     let romanNumeral: string = '';
     let remainValue: number = givenInt;
 
+    /**
+     * The ordering of this array is important - it needs to go from large to small
+     * for the reductive maths to work
+     */
     const intToRom: Array<intRomDef> = [
-        {int:100, rom:'C'}, {int:90, rom:'XC'}, {int:50, rom:'L'}, {int:40, rom:'XL'}, {int:10, rom:'X'}, {int:9, rom:'IX'}, 
-        {int:5, rom:'V'}, {int:4, rom:'IV'}, {int:1, rom:'I'}, 
+        {int:500, rom:'D'}, {int:400, rom:'CD'}, {int:390, rom:'XD'}, {int:100, rom:'C'}, {int:90, rom:'XC'}, {int:50, rom:'L'}, 
+        {int:40, rom:'XL'}, {int:10, rom:'X'}, {int:9, rom:'IX'}, {int:5, rom:'V'}, {int:4, rom:'IV'}, {int:1, rom:'I'}, 
     ];
     
     intToRom.forEach(intRom => {
